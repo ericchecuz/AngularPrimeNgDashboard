4c38d0f2ffb5d501a02bc383be3a40b0caca23c8
ericchecuz <eric.checuz96@gmail.com>
2024-03-19 15:59:12 +0100

    wip login & login effect

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+
+import { Component, ElementRef, HostBinding, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+  constructor(private elementRef: ElementRef) { }
+  @ViewChild('cardAnimate') cardLogin!: ElementRef;
+  ngAfterViewInit(): void {
+    const card = this.cardLogin.nativeElement;
+    console.log(card);
+    setTimeout(() => {
+      card.classList.add('animate');
 
+      console.log(this.elementRef.nativeElement);
+    }, 1100);
+    setTimeout(() => {
+      card.classList.add('bounce');
+    }, 1600);
+
+
+
+  }
 }
